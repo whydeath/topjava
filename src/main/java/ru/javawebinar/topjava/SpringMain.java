6569f1bb184be2cbaeef7987926dad7fe878a807
whydeath <flatpleomax@gmail.com>
2024-11-05 11:51:00 +0300

    Домашняя работа выполнение всех 5пунктов

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -3,41 +3,49 @@ package ru.javawebinar.topjava;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.web.meal.MealRestController;
-import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
         // Инициализируем Spring контекст
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(
                 "spring/spring-app.xml");
-        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        MealRestController mealController = appCtx.getBean(MealRestController.class);
+        System.out.println("\n>>> Spring context initialized");
+        System.out.println(">>> Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        // Создаем тестовые данные
-        Meal meal = new Meal(
-                LocalDateTime.of(2024, Month.JANUARY, 30, 10, 0),
-                "Завтрак",
-                500
-        );
+        try {
+            System.out.println("\n>>> Getting bean MealRestController");
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            System.out.println(">>> Successfully got MealRestController: " + mealController);
 
-        // Тестируем создание meal
-        System.out.println("Creating meal...");
-        ResponseEntity<Meal> response = mealController.createWithLocation(meal);
-        Meal created = response.getBody();
-        System.out.println("Created meal: " + created);
+            // Тестируем получение всех meal
+            System.out.println("\n>>> Getting all meals");
+            List<MealTo> all = mealController.getAll();
+            System.out.println(">>> All meals: " + all);
 
-        // Тестируем получение meal
-        System.out.println("Getting meal...");
-        Meal gotten = mealController.get(created.getId());
-        System.out.println("Retrieved meal: " + gotten);
+            // Тестируем get с несуществующим id
+            System.out.println("\n>>> Getting meal with id 1");
+            try {
+                Meal meal = mealController.get(1);
+                System.out.println(">>> Retrieved meal: " + meal);
+            } catch (Exception e) {
+                System.out.println(">>> Expected exception for not found meal: " + e.getMessage());
+            }
+
+        } catch (Exception e) {
+            System.err.println(">>> Exception occurred:");
+            e.printStackTrace();
+        }
 
         // Закрываем контекст
         appCtx.close();
+        System.out.println("\n>>> Spring context closed");
     }
 }
